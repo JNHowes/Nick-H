2b05eacdfd9bca653d5b99a94013b32a9b6d325a
Nick-H <jeffreynhowes@gmail.com>
2023-04-05 13:30:08 -0700

    animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Nick Howes
+# GitHub username: Nick-H
+# Date: 04/05/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("tiger")
+print("Your favorite animal is the " + fave_animal + ".")
