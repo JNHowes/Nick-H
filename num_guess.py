e7da965d3ffef892a3a3c09e315a9ef8efce91d2
Nick-H <jeffreynhowes@gmail.com>
2023-04-20 12:59:20 -0700

    animal.py

--- num_guess.py
+++ num_guess.py
@@ -1,24 +1,19 @@
 import random
 
-# Generate a random integer between -100 and 100
 target_num = random.randint(-100, 100)
+num_guesses = 0
 
 print("Enter the integer for the player to guess.")
 print(target_num)
 
-guess = None
-num_guesses = 0
-
-# Loop until the user guesses the number
-while guess != target_num:
-    print("Enter your guess.")
-    guess = int(input())
+while True:
+    num_guesses += 1
+    guess = int(input("Enter your guess: "))
 
     if guess > target_num:
-        print("Too high - try again:")
+        print("Too high - try again.")
     elif guess < target_num:
-        print("Too low - try again:")
-
-    num_guesses += 1
-
-print("You guessed it in", num_guesses, "tries.")
+        print("Too low - try again.")
+    else:
+        print("You guessed it in", num_guesses, "tries.")
+        break
