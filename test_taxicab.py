4685648d7a759a152c3b2269f90b00daf58459ce
Nick-H <jeffreynhowes@gmail.com>
2023-05-03 18:26:07 -0700

    animal.py

--- test_taxicab.py
+++ test_taxicab.py
@@ -4,35 +4,69 @@
 # Description: Taxicab with three private data members:
 # 1. Current X coordinate, 2. Current Y coordinate, 3. Current Odometer.
 
-def __init__(self, x_coord, y_coord):
-    """Initialize the Taxicab object with the given x and y coordinates, and initialize odometer to zero."""
-    self._x_coord = x_coord
-    self._y_coord = y_coord
-    self._odometer = 0
+class Taxicab:
+    """
+    A class that represents a taxicab with current x-coordinate, y-coordinate, and odometer reading.
+    """
 
+    def __init__(self, x_coord, y_coord):
+        """
+        Initializes the taxicab's x-coordinate, y-coordinate, and odometer reading.
 
-def get_x_coord(self):
-    """Return the current x-coordinate of the Taxicab."""
-    return self._x_coord
+        Parameters:
+        x_coord (int): The initial x-coordinate.
+        y_coord (int): The initial y-coordinate.
+        """
+        self.__x_coord = x_coord
+        self.__y_coord = y_coord
+        self.__odometer = 0
 
+    def get_x_coord(self):
+        """
+        Returns the current x-coordinate of the taxicab.
 
-def get_y_coord(self):
-    """Return the current y-coordinate of the Taxicab."""
-    return self._y_coord
+        Returns:
+        int: The current x-coordinate.
+        """
+        return self.__x_coord
 
+    def get_y_coord(self):
+        """
+        Returns the current y-coordinate of the taxicab.
 
-def get_odometer(self):
-    """Return the current odometer reading of the Taxicab."""
-    return self._odometer
+        Returns:
+        int: The current y-coordinate.
+        """
+        return self.__y_coord
 
+    def get_odometer(self):
+        """
+        Returns the current odometer reading of the taxicab.
 
-def move_x(self, distance):
-    """Move the Taxicab by the given distance along the x-axis (positive for right, negative for left)."""
-    self._x_coord += distance
-    self._odometer += abs(distance)
+        Returns:
+        int: The current odometer reading.
+        """
+        return self.__odometer
 
+    def move_x(self, distance):
+        """
+        Moves the taxicab left or right by a given distance.
 
-def move_y(self, distance):
-    """Move the Taxicab by the given distance along the y-axis (positive for up, negative for down)."""
-    self._y_coord += distance
-    self._odometer += abs(distance)
+        Parameters:
+        distance (int): The distance to move the taxicab. Positive values move the taxicab right,
+                        while negative values move the taxicab left.
+        """
+        self.__x_coord += distance
+        self.__odometer += abs(distance)
+
+    def move_y(self, distance):
+        """
+        Moves the taxicab up or down by a given distance.
+
+        Parameters:
+        distance (int): The distance to move the taxicab. Positive values move the taxicab up,
+                        while negative values move the taxicab down.
+        """
+        self.__y_coord += distance
+        self.__odometer += abs(distance)
+
